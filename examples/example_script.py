6cc768ab82c839edd4db1bd555554037361e102b
Brian McFee <brian.mcfee@nyu.edu>
2014-11-14 15:10:54 -0500

    updated script

--- examples/example_script.py
+++ examples/example_script.py
@@ -15,9 +15,9 @@ from IPython.parallel import Client, require
 def demo(n):
 
     time.sleep(0.01)
-    return 'Job {:3d}: {:s}\t{:10d}'.format(n,
-                                            '-'.join(os.uname()),
-                                            os.getpid())
+    return "Job {:3d}: {:s}\t{:10d}\n".format(n,
+                                              '-'.join(os.uname()),
+                                              os.getpid())
 
 
 if __name__ == "__main__":
@@ -42,20 +42,3 @@ if __name__ == "__main__":
     results = view.map(demo, range(128))
 
     print(results)
-
-#     jobs = [(i, pool.apply(demo, i)) for i in range(128)]
-
-#     retrieved = [False] * len(jobs)
-
-#     while not all(retrieved):
-#         for i, (fn, j) in enumerate(jobs):
-#             if j.ready() and not retrieved[i]:
-#                 try:
-#                     j.get()
-#                 except Exception as e:
-#                     print("Task failed: {0}".format(fn))
-#                     print("With error: \n{0}".format(e))
-#                 else:
-#                     print("Finished: {0}".format(fn))
-#                 retrieved[i] = True
-#         time.sleep(1)
