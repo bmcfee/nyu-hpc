6c5fb2fb895eedcf54113425395d61178debea3e
Brian McFee <brian.mcfee@nyu.edu>
2014-11-14 14:27:45 -0500

    adding example job

---
+++ examples/example.q
@@ -0,0 +1,44 @@
+#!/bin/bash
+#PBS -l nodes=3:ppn=4
+#PBS -l walltime=12:00:00
+#PBS -l mem=350GB
+#PBS -N demo_script
+#PBS -m ae
+#PBS -M brian.mcfee@nyu.edu
+#PBS -j oe
+
+module purge
+module add mpi4py scikit-learn theano pytables
+
+# Needed environment variables.
+# Locations.
+export SRCDIR=$HOME/git/nyu-hpc/examples
+export RUNDIR={results_root}
+export PROFILEDIR=$RUNDIR/profile
+mkdir -p $RUNDIR
+cd $RUNDIR
+
+# Set up and start the IPython cluster.
+cp -r $HOME/.ipython/profile_mpi $PROFILEDIR
+ipcluster start -n $PBS_NP --profile-dir=$PROFILEDIR &> ipcluster.log &
+
+sleep 5
+for (( try=0; try < 100; ++try )); do
+    if cat ipcluster.log | grep -q "Engines appear to have started successfully"; then
+        success=1
+        break
+    fi
+    sleep 5
+done
+
+if (( success )); then
+    # Run the analysis.
+    python $SRCDIR/example_script.py --profile-dir $PROFILEDIR &> output.log
+else
+    echo "Server never started" &> output.log
+fi
+
+# Shut the cluster down.
+ipcluster stop --profile-dir=$PROFILEDIR
+
+exit $(( 1-success ));
---
+++ examples/example_script.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+
+import sys
+import argparse
+
+import os
+import time
+from IPython.parallel import Client, require
+
+
+@require(os, time)
+def demo(n):
+
+    print('Job {:3d}: {:s}\t{:10d}'.format(n,
+                                           '-'.join(os.uname()),
+                                           os.getpid()))
+
+    time.sleep(0.01)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-p", "--profile-dir", default=None,
+                        help="the IPython profile dir")
+
+    args = parser.parse_args()
+
+    print("Running with the following arguments:")
+    print("sys.argv:")
+    print(sys.argv)
+    print("args:")
+    print(args)
+
+    c = Client(profile_dir=args.profile_dir)
+
+    pool = c.load_balanced_view()
+
+    jobs = [(i, pool.apply(demo, i)) for i in range(128)]
+
+    retrieved = [False] * len(jobs)
+
+    while not all(retrieved):
+        for i, (fn, j) in enumerate(jobs):
+            if j.ready() and not retrieved[i]:
+                try:
+                    j.get()
+                except Exception as e:
+                    print("Task failed: {0}".format(fn))
+                    print("With error: \n{0}".format(e))
+                else:
+                    print("Finished: {0}".format(fn))
+                retrieved[i] = True
+        time.sleep(1)
